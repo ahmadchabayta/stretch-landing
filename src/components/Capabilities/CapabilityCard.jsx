735d1c20272ed6069f6bc91d7c6c584e6c097106
AhmedChabayta <chabays@gmail.com>
2026-01-15 08:53:27 +0200

    Update project with RTL support and various improvements

--- src/components/Capabilities/CapabilityCard.jsx
+++ src/components/Capabilities/CapabilityCard.jsx
@@ -25,7 +25,7 @@ const CapabilityCard = ({ card, isActive, onClick }) => (
       </Typography>
     </div>
     <GlassCircle
-      className="pointer-events-none absolute bottom-0 left-1/2 z-10 h-[85px] w-[85px] -translate-x-1/2 -translate-y-1/2 lg:h-[163px] lg:w-[163px]"
+      className="pointer-events-none absolute bottom-0 left-1/2 right-1/2 z-10 h-[85px] w-[85px] -translate-x-1/2 -translate-y-1/2 rtl:translate-x-1/2 lg:h-[163px] lg:w-[163px]"
       blur={15}
       noiseIntensity={0.1}
       distortionScale={8}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,14 +9,20 @@ const Hero = ({ id }) => {
   const { language } = useLanguage();
   const sectionLabels = data.languages[language] || data.languages.en;
 
+  const isLTR = language === "en";
+
   return (
     <Section
       id={id}
       style={{
-        backgroundImage: `url(${data.images.hero_img})`,
+        backgroundImage: `url(${sectionLabels.hero_img})`,
         backgroundRepeat: "no-repeat",
       }}
-      className="bg-position-[right_-59px_top_180px] md:bg-position-[left_100%_top_299px] lg:bg-position-[right_-26px_top_363.33px] xl:bg-position-[right_-236px_top_-43px] 2xl:bg-position-[right_-120px_top_10%] 3xl:bg-position-[right_-50px_top_110px] bg-size-[663.568px_auto] md:bg-size-[952px_auto] lg:bg-size-[1352px_auto] 3xl:bg-size-[1364px_auto] relative bg-white!"
+      className={`${
+        isLTR
+          ? "bg-position-[right_-59px_top_180px] md:bg-position-[left_100%_top_299px] lg:bg-position-[right_-26px_top_363.33px] xl:bg-position-[right_-236px_top_-43px] 2xl:bg-position-[right_-120px_top_10%] 3xl:bg-position-[right_-50px_top_110px]"
+          : "bg-position-[left_-19px_top_180px] md:bg-position-[right_84%_top_299px] lg:bg-position-[left_-41px_top_363.33px] xl:bg-position-[left_-40px_top_-43px] 2xl:bg-position-[left_-120px_top_10%] 3xl:bg-position-[left_-50px_top_110px]"
+      } bg-size-[663.568px_auto] md:bg-size-[952px_auto] lg:bg-size-[1352px_auto] 3xl:bg-size-[1364px_auto] relative bg-white!`}
     >
       <Container className="3xl:pt-[243px] 2xl:pt-[283px] xl:pt-[209px] lg:pt-[57px]">
         <div className="h-full flex flex-col items-center justify-center xl:items-start xl:justify-center text-center xl:text-start">
