736bf8c0a2ff55f184303d9d45f8f04155c760d6
ahmadchabayta <a.chabayta@memob.com>
2026-01-23 10:13:06 +0200

    chore: update notes and Q&A on email sending options for DemoModal

--- src/components/StretchUserExperience/StretchUserExperience.jsx
+++ src/components/StretchUserExperience/StretchUserExperience.jsx
@@ -47,7 +47,7 @@ const StretchUserExperience = () => {
   return (
     <Section
       container={false}
-      className="stretch_user_experience relative w-full overflow-hidden  flex flex-col"
+      className="stretch_user_experience relative w-full overflow-hidden flex flex-col"
       style={{
         backgroundImage: `url('${withBase(backgroundImage)}')`,
         backgroundRepeat: "no-repeat",
@@ -61,15 +61,15 @@ const StretchUserExperience = () => {
         alt="Blue Circle"
       />
 
-      <Container className="relative z-10 pt-4 md:pt-6 shrink-0">
+      <Container className="relative z-10 shrink-0">
         <SectionTitle currentStep={currentIndex + 1} data={sectionLabels} />
       </Container>
 
       {/* Carousel Section - Centered in Container */}
-      <Container className="xl:absolute xl:top-1/2 xl:translate-y-[20%] xl:left-1/2 xl:translate-x-[-50%] flex flex-col xl:flex-row xl:items-center justify-end xl:justify-center gap-6 lg:gap-10 flex-1">
+      <Container className="xl:min-h-screen flex flex-col xl:flex-row xl:items-center justify-end xl:justify-center gap-6 lg:gap-10 flex-1 pb-12">
         {/* 3D Carousel */}
         <div
-          className="relative w-full flex-1 max-h-[300px] md:max-h-[350px] lg:max-h-[450px] xl:max-h-[800px] flex items-center justify-center origin-center overflow-visible"
+          className="relative w-full flex-1 flex items-center justify-center origin-center overflow-visible"
           style={{ perspective: "1000px" }}
         >
           {items.map((item, index) => {
